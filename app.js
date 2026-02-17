4b6799f559be6df4445ab31fae9a8f57fa288d15
Siddhijape <japesiddhi2@gmail.com>
2026-02-16 21:45:42 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const nextbutton=document.querySelector(".next-btn");
 const video=document.querySelector(".hero-video");
 
-const movielist = ["hero-1.mp4","hero-2.mp4","hero-3.mp4","hero-4.mp4"];
+const movielist = ["Videos/hero-1.mp4","Videos/hero-2.mp4","Videos/hero-3.mp4","Videos/hero-4.mp4"];
 
 let index =0;
 nextbutton.addEventListener('click',function(){
@@ -11,4 +11,5 @@ nextbutton.addEventListener('click',function(){
     if(index === 3){
         index = -1;
     }
+
 })
